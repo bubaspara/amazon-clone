c0f37b99a118a4bd4caff0fc7302a46362178da1
Mirko Sisko <mirko.sisko@gmail.com>
2022-03-07 17:15:34 +0100

    Inital commit, added Header

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC45w3yEFfQGt6iP0ANQtRaiytcFmss2pw",
+  authDomain: "clone-862a0.firebaseapp.com",
+  projectId: "clone-862a0",
+  storageBucket: "clone-862a0.appspot.com",
+  messagingSenderId: "843911662489",
+  appId: "1:843911662489:web:862ae3ea4aea0d89ab6108",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
